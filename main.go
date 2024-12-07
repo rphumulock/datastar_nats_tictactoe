9012226f41910b7b7473d569179bab6ac98e6f56
Peter Humulock <rphumulock@gmail.com>
2024-12-07 00:44:47 -0500

    Initial commit

---
+++ main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/zangster300/northstar/routes"
+	"golang.org/x/sync/errgroup"
+)
+
+func main() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
+	getPort := func() string {
+		if p, ok := os.LookupEnv("PORT"); ok {
+			return p
+		}
+		return "8080"
+	}
+	logger.Info(fmt.Sprintf("Starting Server 0.0.0.0:" + getPort()))
+	defer logger.Info("Stopping Server")
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	if err := run(ctx, logger, getPort()); err != nil {
+		logger.Error("Error running server", slog.Any("err", err))
+		os.Exit(1)
+	}
+}
+
+func run(ctx context.Context, logger *slog.Logger, port string) error {
+	g, ctx := errgroup.WithContext(ctx)
+
+	g.Go(startServer(ctx, logger, port))
+
+	if err := g.Wait(); err != nil {
+		return fmt.Errorf("error running server: %w", err)
+	}
+
+	return nil
+}
+
+func startServer(ctx context.Context, logger *slog.Logger, port string) func() error {
+	return func() error {
+		router := chi.NewMux()
+
+		router.Use(
+			middleware.Logger,
+			middleware.Recoverer,
+		)
+
+		router.Handle("/static/*", http.StripPrefix("/static/", static(logger)))
+
+		cleanup, err := routes.SetupRoutes(ctx, logger, router)
+		defer cleanup()
+		if err != nil {
+			return fmt.Errorf("error setting up routes: %w", err)
+		}
+
+		srv := &http.Server{
+			Addr:    "0.0.0.0:" + port,
+			Handler: router,
+		}
+
+		go func() {
+			<-ctx.Done()
+			srv.Shutdown(context.Background())
+		}()
+
+		return srv.ListenAndServe()
+	}
+}
